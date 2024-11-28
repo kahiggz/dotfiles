7667eae5186dbaff6d09d4ed278d14861f1620e7
kahiggz <bryan.kahiga@gmail.com>
2024-11-28 12:14:05 +0300

    updated harpoon and configs

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -2,13 +2,14 @@ return {
   "ThePrimeagen/harpoon",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
   },
   config = function()
-    local telescope = require("telescope")
-
     -- Harpoon setup
     require("harpoon").setup({
+      -- To see more options, visit :help harpoon
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
       global_settings = {
         save_on_toggle = false,
         save_on_change = true,
@@ -22,17 +23,14 @@ return {
       },
     })
 
-    -- Load Harpoon extension for Telescope
-    telescope.load_extension("harpoon")
-
-    -- Set keymaps
+    -- Set keymaps for Harpoon actions
     local keymap = vim.keymap
 
     keymap.set(
       "n",
       "<leader>ho",
-      "<cmd>lua require('telescope').extensions.harpoon.marks()<cr>",
-      { desc = "Open Harpoon menu in Telescope" }
+      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
+      { desc = "Open Harpoon menu" }
     )
     keymap.set(
       "n",
@@ -46,6 +44,12 @@ return {
       "<leader>hp",
       "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
       { desc = "Go to previous Harpoon mark" }
+    )
+    keymap.set(
+      "n",
+      "<leader>hr",
+      "<cmd>lua require('harpoon.mark').rm_file()<cr>",
+      { desc = "Remove current file from Harpoon" }
     )
   end,
 }
